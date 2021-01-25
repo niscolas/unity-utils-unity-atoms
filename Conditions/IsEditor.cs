7639a95d12564a0273829507c52dfe9eb5772ee3
Nícolas Catarina Parreiras <nic.cp@hotmail.com>
2021-01-25 10:30:20 -0300

    adds new utilities

---
+++ Conditions/IsEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityAtoms;
+using UnityEngine;
+
+namespace Plugins.UnityAtomsUtils.Conditions {
+	[EditorIcon("atom-icon-teal")]
+	[CreateAssetMenu(menuName = AtomsConstants.ConditionsCreateAssetMenuPath + "() => Is Editor")]
+	public class IsEditor : VoidCondition {
+		[SerializeField]
+		private bool checkIfIsNotEditor;
+
+		public override bool Call() {
+			return checkIfIsNotEditor ? !Application.isEditor : Application.isEditor;
+		}
+	}
+}
---
+++ Functions/ScreenToWorldPoint2d.cs
@@ -0,0 +1,24 @@
+﻿using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Plugins.UnityAtomsUtils.Functions {
+	[EditorIcon("atom-icon-sand")]
+	[CreateAssetMenu(menuName =
+		AtomsConstants.FunctionsCreateAssetMenuPath + "(" + nameof(Vector2) + ") => Screen To World Point")]
+	public class ScreenToWorldPoint2d : Vector2Vector2Function {
+		private Camera mainCamera;
+
+		public override Vector2 Call(Vector2 parameter) {
+			if (mainCamera == null) {
+				mainCamera = Camera.main;
+			}
+
+			if (mainCamera == null) {
+				return Vector2.zero;
+			}
+
+			return mainCamera.ScreenToWorldPoint(parameter);
+		}
+	}
+}
