f112ab28fe61b925d96732f4a91b6c8729a18948
Nícolas Catarina Parreiras <nic.cp@hotmail.com>
2021-02-02 19:29:57 -0300

    removes SoundPreset atoms

--- Actions/SoundPresetPairAction.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityAtoms
-{
-    /// <summary>
-    /// Action of type `SoundPresetPair`. Inherits from `AtomAction&lt;SoundPresetPair&gt;`.
-    /// </summary>
-    [EditorIcon("atom-icon-purple")]
-    public abstract class SoundPresetPairAction : AtomAction<SoundPresetPair> { }
-}
