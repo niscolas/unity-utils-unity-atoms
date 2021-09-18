830e01c89bb4ce881751e2758523d03ced25192b
niscolas <nic.cp@hotmail.com>
2021-09-18 16:28:04 -0300

    updates plugins

--- Scripts/Runtime/Components/ConditionCheckers/BaseConditionChecker.cs
+++ Scripts/Runtime/Components/ConditionCheckers/BaseConditionChecker.cs
@@ -2,55 +2,55 @@
 using UnityAtoms;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace niscolas.UnityUtils.UnityAtoms
 {
-	public class BaseConditionChecker<T, P, C, V, E1, E2, F, VI, TR> : MonoBehaviour
-		where TR : AtomReference<T, P, C, V, E1, E2, F, VI>
-		where P : struct, IPair<T>
-		where C : AtomBaseVariable<T>
-		where V : AtomVariable<T, P, E1, E2, F>
-		where E1 : AtomEvent<T>
-		where E2 : AtomEvent<P>
-		where F : AtomFunction<T, T>
-		where VI : AtomVariableInstancer<V, P, T, E1, E2, F>
-	{
-		[SerializeField]
-		private AtomCondition<T> _condition;
+    public class BaseConditionChecker<T, P, C, V, E1, E2, F, VI, TR> : MonoBehaviour
+        where TR : AtomReference<T, P, C, V, E1, E2, F, VI>
+        where P : struct, IPair<T>
+        where C : AtomBaseVariable<T>
+        where V : AtomVariable<T, P, E1, E2, F>
+        where E1 : AtomEvent<T>
+        where E2 : AtomEvent<P>
+        where F : AtomFunction<T, T>
+        where VI : AtomVariableInstancer<V, P, T, E1, E2, F>
+    {
+        [SerializeField]
+        private AtomCondition<T> _condition;
 
-		[SerializeField]
-		private TR _fixedInput;
+        [SerializeField]
+        private TR _fixedInput;
 
-		[Title("Events")]
-		[SerializeField]
-		private UnityEvent<T> _ifTrue;
+        [Title("Events")]
+        [FormerlySerializedAs("_ifTrue"), SerializeField]
+        private UnityEvent<T> _onConditionMet;
 
-		[SerializeField]
-		private UnityEvent<T> _ifFalse;
+        [FormerlySerializedAs("_ifFalse"), SerializeField]
+        private UnityEvent<T> _onConditionFailed;
 
-		public void Check(T input)
-		{
-			bool conditionResult = _condition.Call(input);
+        public void Check(T input)
+        {
+            if (_condition.Call(input))
+            {
+                _onConditionMet?.Invoke(input);
+            }
+            else
+            {
+                _onConditionFailed?.Invoke(input);
+            }
+        }
 
-			if (conditionResult)
-			{
-				_ifTrue?.Invoke(input);
-				return;
-			}
+        public void Check()
+        {
+            T inputValue = default;
 
-			_ifFalse?.Invoke(input);
-		}
+            if (_fixedInput != null)
+            {
+                inputValue = _fixedInput.Value;
+            }
 
-		public void Check()
-		{
-			T inputValue = default;
-
-			if (_fixedInput != null)
-			{
-				inputValue = _fixedInput.Value;
-			}
-
-			Check(inputValue);
-		}
-	}
+            Check(inputValue);
+        }
+    }
 }
---
+++ Scripts/Runtime/Components/ConditionCheckers/CheckBoolReference.cs
@@ -0,0 +1,31 @@
+﻿using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace niscolas.UnityUtils.UnityAtoms
+{
+    public class CheckBoolReference : MonoBehaviour
+    {
+        [SerializeField]
+        private BoolReference _value;
+
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent _onConditionMet;
+        
+        [SerializeField]
+        private UnityEvent _onConditionFailed;
+
+        public void Check()
+        {
+            if (_value.Value)
+            {
+                _onConditionMet?.Invoke();
+            }
+            else
+            {
+                _onConditionFailed?.Invoke();
+            }
+        }
+    }
+}
