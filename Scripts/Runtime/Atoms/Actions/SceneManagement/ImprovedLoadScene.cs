e2673c256586fa5e558c329f892c709d14986692
niscolas <nic.cp@hotmail.com>
2021-11-23 19:43:11 -0300

    updates ImprovedLoadScene

--- Scripts/Runtime/Atoms/Actions/SceneManagement/ImprovedLoadScene.cs
+++ Scripts/Runtime/Atoms/Actions/SceneManagement/ImprovedLoadScene.cs
@@ -6,23 +6,26 @@ using UnityEngine.Serialization;
 
 namespace niscolas.UnityUtils.UnityAtoms
 {
-	[CreateAssetMenu(
-		menuName = UnityAtomsConstants.SceneManagementCreateAssetMenuPrefix + "Improved Change Scene")]
-	public class ImprovedLoadScene : AtomAction
-	{
-		[FormerlySerializedAs("sceneFieldReference"), SerializeField]
-		private SceneFieldReference _scene;
+    [CreateAssetMenu(
+        menuName = UnityAtomsConstants.SceneManagementCreateAssetMenuPrefix + "Improved Change Scene")]
+    public class ImprovedLoadScene : AtomAction
+    {
+        [FormerlySerializedAs("sceneFieldReference"), SerializeField]
+        private SceneFieldReference _scene;
 
-		[FormerlySerializedAs("loadSceneMode"), SerializeField]
-		private LoadSceneMode _loadSceneMode;
+        [FormerlySerializedAs("loadSceneMode"), SerializeField]
+        private LoadSceneMode _loadSceneMode;
 
-		public override void Do()
-		{
-			bool isSceneLoaded = SceneManager.GetSceneByBuildIndex(_scene.Value.BuildIndex).isLoaded;
+        public override void Do()
+        {
+            bool isSceneLoaded = SceneManager.GetSceneByName(_scene.Value.SceneName).isLoaded;
 
-			if (isSceneLoaded) return;
+            if (isSceneLoaded)
+            {
+                return;
+            }
 
-			SceneManager.LoadScene(_scene.Value.BuildIndex, _loadSceneMode);
-		}
-	}
+            SceneManager.LoadScene(_scene.Value.SceneName, _loadSceneMode);
+        }
+    }
 }
